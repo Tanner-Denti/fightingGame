6da70e301e9b34640863a02f68c7adf80ced59e1
Curtis Nash <curtis.r.nash@gmail.com>
2022-12-04 20:55:25 -0700

    Added font

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,10 +12,23 @@ public class MainMenu : MonoBehaviour
     public AudioClip clickSound;
     public float volume;
     public AudioMixer mixer;
-    public void PlayGame()
+    
+    public void StageOne()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("StageOne");
     }
+    public void StageTwo()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("StageTwo");
+    }
+    public void StageThree()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("StageThree");
+    }
+
 
     public void HoverSound()
     {
