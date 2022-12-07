f7d13bada614dbf9c281dcce34f6b0949bcb2d68
Kupu Tuifua <tui20002@byui.edu>
2022-12-07 09:06:05 -0700

    Finished the status effects as well as potion deletion on collision.

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyObject : MonoBehaviour
+namespace Scripts
 {
-    private void OnTriggerEnter(Collider other)
+    public class DestroyObject : MonoBehaviour
     {
-        Destroy(other.gameObject, 7f);
+        Spawner spawner;
+        private bool shouldDestroy = true;
+        
+        void awake()
+        {
+            spawner = spawner.GetComponent<Spawner>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                shouldDestroy = false;
+            }
+            if(shouldDestroy == true)
+            {
+                Destroy(other.gameObject, 7f);
+            }
+            shouldDestroy = true;
+        }
+        
+        
     }
-    
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,93 +2,115 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+namespace Scripts
 {
-    public GameObject[] potions;
-    public GameObject[] weapons;
-    public float powerup_minTime = 15;
-    public float powerup_maxTime = 30;
-    public float weapon_minTime = 30;
-    public float weapon_maxTime = 60;
-    private bool spawnpowerup;
-    private bool spawnweapon;
-    private bool destroyspawn;
-
-    void Start()
+    public class Spawner : MonoBehaviour
     {
+        private List<GameObject> potions = new List<GameObject>();
+        private List<GameObject> weapons = new List<GameObject>();
+        public GameObject berserkPotion;
+        public GameObject fastPotion;
+        public GameObject jumpPotion;
+        public GameObject healthPotion;
+        public GameObject bow;
+        public GameObject sword;
+        public GameObject shield;
+        private float powerup_minTime = 15;
+        private float powerup_maxTime = 30;
+        private float weapon_minTime = 30;
+        private float weapon_maxTime = 60;
+        private bool spawnpowerup;
+        private bool spawnweapon;
+        private float ranges;
         
-        foreach (GameObject weapon in  weapons)
+
+
+        void Start()
         {
-            if(weapon.GetComponent<Rigidbody>() == null)
+            potions.Add(berserkPotion);
+            potions.Add(fastPotion);
+            potions.Add(jumpPotion);
+            potions.Add(healthPotion);
+            weapons.Add(bow);
+            weapons.Add(shield);
+            weapons.Add(sword);
+            
+
+            
+
+            foreach (GameObject weapon in  weapons)
             {
-                weapon.AddComponent<Rigidbody>();
+                if(weapon.GetComponent<Rigidbody>() == null)
+                {
+                    weapon.AddComponent<Rigidbody>();
+                    
+                }
+                if(weapon.GetComponent<BoxCollider>() == null)
+                {
+                    weapon.AddComponent<BoxCollider>();
+                }
                 
+            
             }
-            if(weapon.GetComponent<BoxCollider>() == null)
+
+            foreach (GameObject potion in  potions)
             {
-                weapon.AddComponent<BoxCollider>();
+                if(potion.GetComponent<Rigidbody>() == null)
+                {
+                    potion.AddComponent<Rigidbody>();
+                }
+                if(potion.GetComponent<BoxCollider>() == null)
+                {
+                    potion.AddComponent<BoxCollider>();
+                }      
             }
-         
         }
 
-        foreach (GameObject potion in  potions)
+        void Update()
         {
-            if(potion.GetComponent<Rigidbody>() == null)
+            if (!spawnpowerup)
             {
-                potion.AddComponent<Rigidbody>();
-            }
-            if(potion.GetComponent<BoxCollider>() == null)
-            {
-                potion.AddComponent<BoxCollider>();
-            }
-        }
-
-    }
-
-    void Update()
-    {
-        if (!spawnpowerup)
-        {
-            float powerup_timer = Random.Range(powerup_minTime, powerup_maxTime);
-    
-            Invoke("MakePowerUp", powerup_timer);
- 
-            spawnpowerup = true;
-        }
-
-        if (!spawnweapon)
-        {
-       
-            float weapon_timer = Random.Range(weapon_minTime, weapon_maxTime);
-            Invoke("MakeWeapon", weapon_timer);
-
-
-            spawnweapon = true;
-        }
-    }
-
-
-    private void MakePowerUp()
-    {
-        int randomIndex = Random.Range(0, potions.Length);
-        Vector3 randomPosition = new Vector3(Random.Range(-10,11), Random.Range(15,16), 0);
-
-        Instantiate(potions[randomIndex], randomPosition, Quaternion.identity);
+                float powerup_timer = Random.Range(powerup_minTime, powerup_maxTime);
         
+                Invoke("MakePowerUp", powerup_timer);
+    
+                spawnpowerup = true;
+            }
 
-        spawnpowerup = false;
+            if (!spawnweapon)
+            {
+        
+                float weapon_timer = Random.Range(weapon_minTime, weapon_maxTime);
+                Invoke("MakeWeapon", weapon_timer);
+
+
+                spawnweapon = true;
+            }
+
+            ranges = Random.Range(-9.00f , 10.00f);
+        }
+
+
+        private void MakePowerUp()
+        {
+            int randomIndex = Random.Range(0, potions.Count);
+            Vector3 randomPosition = new Vector3(ranges, 15 , 0);
+
+            Instantiate(potions[randomIndex], randomPosition, Quaternion.identity);
+            
+
+            spawnpowerup = false;
+        }
+
+        private void MakeWeapon()
+        {
+            int randomIndex = Random.Range(0, weapons.Count);
+            Vector3 randomPosition = new Vector3(ranges, 15, 0);
+
+            Instantiate(weapons[randomIndex], randomPosition, Quaternion.identity);
+
+        
+            spawnweapon = false;
+        }
     }
-
-    private void MakeWeapon()
-    {
-        int randomIndex = Random.Range(0, weapons.Length);
-        Vector3 randomPosition = new Vector3(Random.Range(-10, 11), Random.Range(15, 16), 0);
-
-        Instantiate(weapons[randomIndex], randomPosition, Quaternion.identity);
-
-       
-        spawnweapon = false;
-    }
-
-
 }
