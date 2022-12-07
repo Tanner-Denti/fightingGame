bbd27a500b3b4a08392aac9b423b919b9fbfea56
Kupu Tuifua <tui20002@byui.edu>
2022-12-06 17:57:18 -0700

    Added the spawner and destroyer scripts and objects, as well as the weapons and potions.

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject, 7f);
+    }
+    
+}
